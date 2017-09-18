98cc5f66d877b7ed9d38f8f4fa2d5f50ddfbca38
puzcoin <akuma@mud.com.cn>
2017-09-18 11:05:22 +0800

    getof

--- src/masternode.cpp
+++ src/masternode.cpp
@@ -267,8 +267,9 @@ void CMasternode::Check(bool fForce)
             return;
         }
     }
-
-    if(lastPing.sigTime - sigTime < MASTERNODE_MIN_MNP_SECONDS) {
+// FIXME by akuma
+// i // this first to see weather the masternode can startup
+/*    if(lastPing.sigTime - sigTime < MASTERNODE_MIN_MNP_SECONDS) {
 LogPrintf("PRE_ENABLED:%d - %d < %d\n",lastPing.sigTime,sigTime,MASTERNODE_MIN_MNP_SECONDS);
         nActiveState = MASTERNODE_PRE_ENABLED;
         if(nActiveStatePrev != nActiveState) {
@@ -276,7 +277,7 @@ LogPrintf("PRE_ENABLED:%d - %d < %d\n",lastPing.sigTime,sigTime,MASTERNODE_MIN_M
         }
         return;
     }
-
+*/
     nActiveState = MASTERNODE_ENABLED; // OK
     if(nActiveStatePrev != nActiveState) {
         LogPrint("masternode", "CMasternode::Check -- Masternode %s is in %s state now\n", vin.prevout.ToStringShort(), GetStateString());

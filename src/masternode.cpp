c66dde888480f49b0dfa71e9d6fc7552915868e2
puzcoin <akuma@mud.com.cn>
2017-09-18 17:23:19 +0800

    5

--- src/masternode.cpp
+++ src/masternode.cpp
@@ -2,6 +2,7 @@
 // Distributed under the MIT/X11 software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 //FIXME:akuma try to sync masternode sigTime
+//
 #include "activemasternode.h"
 #include "consensus/validation.h"
 #include "darksend.h"

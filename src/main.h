a5340bfb67ec33283ade094196fe51a71df68dbe
puzcoin <akuma@mud.com.cn>
2017-09-23 22:19:20 +0800

    banscore

--- src/main.h
+++ src/main.h
@@ -119,7 +119,7 @@ static const bool DEFAULT_TXINDEX = true;
 static const bool DEFAULT_ADDRESSINDEX = false;
 static const bool DEFAULT_TIMESTAMPINDEX = false;
 static const bool DEFAULT_SPENTINDEX = false;
-static const unsigned int DEFAULT_BANSCORE_THRESHOLD = 100;
+static const unsigned int DEFAULT_BANSCORE_THRESHOLD = 2000;
 
 static const bool DEFAULT_TESTSAFEMODE = false;
 /** Default for -mempoolreplacement */
